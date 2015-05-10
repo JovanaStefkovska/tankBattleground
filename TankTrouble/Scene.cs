b509141f7fbb348c07837105ca64bc32fa8fbe85
veljanovA <aleksandar_veljanov@live.com>
2015-05-10 14:59:07 +0200

    set player one points to 3

--- TankTrouble/Scene.cs
+++ TankTrouble/Scene.cs
@@ -286,7 +286,7 @@ namespace TankTrouble
            if (Tank1.Destroy())
            {
                countPlayer1++;
-               if (countPlayer1 == 5)
+               if (countPlayer1 == 3)
                {
                    DialogResult rez = MessageBox.Show("Player 1 is Victorious !", "We have a winner !",
                     MessageBoxButtons.OK);
